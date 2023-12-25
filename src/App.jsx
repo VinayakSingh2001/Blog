7f4d6267b6c3aa4b884ef0905d1ced30a97ad53b
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-25 09:22:48 +0530

    added nav

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Navbar } from './components'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+
+  )
+}
+
+export default App
