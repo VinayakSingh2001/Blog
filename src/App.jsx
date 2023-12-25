a9e4ffa2d88be9edd54535544b76c87b31dcd2a0
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-25 12:56:03 +0530

    Routing Between Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import { Homepage, BlogContentPage } from './pages'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      {/* <Homepage /> */}
-      <BlogContentPage />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/blog/:id' element={<BlogContentPage />}/>
+      </Routes>
+      
+      
     </div>
 
   )
