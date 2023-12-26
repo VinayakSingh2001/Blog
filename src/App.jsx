80940efc61eaea9ae964797801e96488ca3e7a05
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-12-26 06:44:06 +0530

    Fetch data via API

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import { Homepage, BlogContentPage } from './pages'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import useFetch from './Hooks/useFetch'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  let { loading, error, data } = useFetch('http://localhost:1337/api/blogs?populate=*')
+  if (loading) return <p>loading...</p>
+  if (error) return <p>Error!</p>
+  console.log(data);
 
   return (
     <div>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/blog/:id' element={<BlogContentPage />}/>
+        <Route path='/blog/:id' element={<BlogContentPage />} />
       </Routes>
-      
-      
+
+
     </div>
 
   )
